b13ff16f9cdf6cbda8f2950e7c6698c53d0be9e0
Abiola Ibrahim <git@abiosoft.com>
2025-03-17 14:22:04 +0100

    render: utilise in-built template nesting
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- mold.go
+++ mold.go
@@ -1,25 +1,11 @@
 package mold
 
 import (
-	"bytes"
 	"fmt"
 	"html/template"
 	"io"
 	"io/fs"
 )
-
-// New creates a new layout using the undelying filesystem.
-func New(fs fs.FS, layoutFile string) (Layout, error) {
-	l, err := newTemplate(fs, layoutFile)
-	if err != nil {
-		return nil, fmt.Errorf("error creating new layout: %w", err)
-	}
-
-	return &tplLayout{
-		l:  l,
-		fs: fs,
-	}, nil
-}
 
 // Layout represents a web page structure, incorporating a specific view.
 // It encapsulates the header and body and inserts the dynamic content of a view
@@ -38,6 +24,24 @@ type Layout interface {
 	Render(w io.Writer, view string, data any) error
 }
 
+// New creates a new layout using fs as the underlying filesystem.
+func New(fs fs.FS, layoutFile string) (Layout, error) {
+	f, err := readFile(fs, layoutFile)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new layout: %w", err)
+	}
+
+	l, err := template.New("layout").Parse(f)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new layout: %w", err)
+	}
+
+	return &tplLayout{
+		l:  l,
+		fs: fs,
+	}, nil
+}
+
 type tplLayout struct {
 	fs fs.FS
 	l  *template.Template
@@ -45,69 +49,54 @@ type tplLayout struct {
 
 // Render implements Layout.
 func (t *tplLayout) Render(w io.Writer, view string, data any) error {
-	tpl, err := parseView(t.fs, view)
+	l, err := t.parseView(t.fs, view)
 	if err != nil {
 		return err
 	}
 
-	head, err := execTemplate(tpl.Lookup("head"), data)
-	if err != nil {
-		return fmt.Errorf("error executing template head for view '%s': %w", view, err)
+	layoutData := map[string]any{
+		"data": data,
 	}
 
-	body, err := execTemplate(tpl, data)
-	if err != nil {
-		return fmt.Errorf("error executing template body for view '%s': %w", view, err)
-	}
-
-	layoutData := map[string]template.HTML{
-		"head": head,
-		"body": body,
-	}
-
-	if err := t.l.Execute(w, layoutData); err != nil {
+	if err := l.Execute(w, layoutData); err != nil {
 		return fmt.Errorf("error rendering layout with view '%s': %w", view, err)
 	}
 
 	return nil
 }
 
-func newTemplate(fs fs.FS, name string) (*template.Template, error) {
+func readFile(fs fs.FS, name string) (string, error) {
 	f, err := fs.Open(name)
 	if err != nil {
-		return nil, fmt.Errorf("error reading file: %w", err)
+		return "", fmt.Errorf("error reading file: %w", err)
 	}
 	b, err := io.ReadAll(f)
 	if err != nil {
-		return nil, fmt.Errorf("error reading file: %w", err)
+		return "", fmt.Errorf("error reading file: %w", err)
 	}
 
-	tpl, err := template.New("").Parse(string(b))
-	if err != nil {
-		return nil, fmt.Errorf("error parseing template '%s': %w", name, err)
-	}
-	return tpl, nil
+	return string(b), nil
 }
 
-func parseView(fs fs.FS, name string) (*template.Template, error) {
-	tpl, err := newTemplate(fs, name)
+func (t *tplLayout) parseView(fs fs.FS, name string) (*template.Template, error) {
+	l, err := t.l.Clone()
 	if err != nil {
+		return nil, fmt.Errorf("error cloning layout for view '%s': %w", name, err)
+	}
+
+	f, err := readFile(fs, name)
+	if err != nil {
+		return nil, fmt.Errorf("error reading view '%s': %w", name, err)
+	}
+
+	if _, err := l.New("body").Parse(f); err != nil {
 		return nil, fmt.Errorf("error parsing view '%s': %w", name, err)
 	}
 
 	// check for head or put an empty placeholder if missing
-	if tpl.Lookup("head") == nil {
-		tpl.New("head").Parse("")
+	if l.Lookup("head") == nil {
+		l.New("head").Parse("")
 	}
 
-	return tpl, nil
-}
-
-func execTemplate(t *template.Template, data any) (template.HTML, error) {
-	var buf bytes.Buffer
-	if err := t.Execute(&buf, data); err != nil {
-		return "", fmt.Errorf("error executing template: %w", err)
-	}
-
-	return template.HTML(buf.String()), nil
+	return l, nil
 }

ec51c50dc53d624af7fde2425825f8b6b4fccc3d
Abiola Ibrahim <git@abiosoft.com>
2025-03-20 17:13:16 +0100

    chore: update example
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

---
+++ examples/basic/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/abiosoft/mold"
+)
+
+//go:embed *.html partials
+var dir embed.FS
+var layout = mold.Must(mold.New(dir))
+
+func main() {
+	handler := http.HandlerFunc(handler)
+
+	http.Handle("/", handler)
+	http.Handle("/hello", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Name string
+	}
+	if strings.HasPrefix(r.URL.Path, "/hello") {
+		data.Name = r.FormValue("name")
+		if data.Name == "" {
+			data.Name = "friend"
+		}
+	}
+
+	if err := layout.Render(w, "index.html", data); err != nil {
+		log.Println("error during render:", err)
+	}
+}

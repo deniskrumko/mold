bfe1a538c84d88c2a7728c5404453ce6cdc9728b
Abiola Ibrahim <git@abiosoft.com>
2025-03-22 07:06:15 +0100

    docs: update package documentation
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- doc.go
+++ doc.go
@@ -1,35 +1,67 @@
-// Package mold provides a flexible web page rendering engine that
-// incorporates layouts and views for structured HTML output.
-//
-// The core component of this package is the [Engine], which
-// manages the rendering process. The engine is configured using [Option].
-//
-// The [Engine] combines layouts and views to produce complete HTML pages.
-// Layouts define the top-level structure, such as headers, footers, and
-// navigation, while views provide the dynamic content for specific pages.
-//
-// [Engine] also supports partial templates, which can be included within both layouts and views.
-//
-// Example Usage:
-//
-//	dir := os.DirFS("web")
-//
-//	options := mold.With(
-//	    mold.WithRoot("templates"),
-//	    mold.WithLayout("layout.html"),
-//	}
-//
-//	engine, err := mold.New(dir, options)
-//	if err != nil {
-//	    // handle error
-//	}
-//
-//	data := map[string]any{
-//	    "Title":   "My Page",
-//	    "Content": "Welcome to my website!",
-//	}
-//
-//	handler := func(w http.ResponseWriter, r *http.Request) {
-//	   engine.Render(w, "view.html", data)
-//	}
+/*
+Package mold provides a flexible web page rendering engine that
+combines layouts and views for structured HTML output.
+
+The core component of this package is the [Engine], which manages the rendering process.
+
+	dir := os.DirFS("web")
+
+	engine, err := mold.New(dir)
+	if err != nil {
+	    // handle error
+	}
+
+	data := map[string]any{
+	    "Title":   "My Page",
+	    "Content": "Welcome to my website!",
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+	   engine.Render(w, "view.html", data)
+	}
+
+Layouts define the top-level structure, such as headers, footers, and
+navigation.
+
+Inside a layout, calling "render" without an argument inserts the view's content into the layout's body.
+To render a specific section, pass the section's name as an argument.
+
+	<!DOCTYPE html>
+	<html>
+
+	<head>
+	    {{ render "head" }}
+	</head>
+
+	<body>
+	    {{ render }}
+	</body>
+
+	</html>
+
+Views are templates that generate the content that is inserted into the body of layouts.
+
+Sections can also be defined within views with the "define" block, allowing content to be rendered in specific parts
+of the layout.
+
+	{{define "head"}}
+	<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+	{{end}}
+
+	<h3>Hello</h3>
+
+	<p>This is a simple web page.</p>
+
+Partials are reusable template snippets that allow you to break down complex views into smaller,
+manageable components. They are supported in both views and layouts with the "partial" function.
+
+Partials are ideal for sharing common logic across multiple views and layouts.
+
+	{{ partial "path/to/partial.html" }}
+
+An optional second argument allows customizing the data passed to the partial.
+By default, the view's data context is used.
+
+	{{ partial "partials/user_session.html" .User }}
+*/
 package mold

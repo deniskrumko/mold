b6510f2bdbb96a96bd17bb43b60f7a713155c9ce
Abiola Ibrahim <git@abiosoft.com>
2025-03-22 14:15:20 +0100

    tests: add unit test for filename extension validation
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

---
+++ engine_test.go
@@ -0,0 +1,109 @@
+package mold
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestValidExt(t *testing.T) {
+	tests := []struct {
+		exts     []string
+		ext      string
+		expected bool
+	}{
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      ".txt",
+			expected: true,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "txt",
+			expected: true,
+		},
+		{
+			exts:     []string{"txt", "go", "html"},
+			ext:      ".txt",
+			expected: true,
+		},
+		{
+			exts:     []string{"txt", "go", "html"},
+			ext:      "txt",
+			expected: true,
+		},
+		{
+			exts:     []string{".TXT", ".Go", ".HTML"},
+			ext:      ".txt",
+			expected: true,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      ".js",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "js",
+			expected: false,
+		},
+		{
+			exts:     []string{},
+			ext:      ".txt",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      ".",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "...",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      ".txt.js",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "txt.js",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      ".txt.",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html"},
+			ext:      "txt.",
+			expected: false,
+		},
+		{
+			exts:     []string{".txt", ".go", ".html", ".tXt"},
+			ext:      ".tXt",
+			expected: true,
+		},
+		{
+			exts:     []string{".txt", "go", ".html", ".tXt"},
+			ext:      "tXt",
+			expected: true,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := validExt(tt.exts, tt.ext)
+			if result != tt.expected {
+				t.Errorf("validExt(%v, %q) = %v, expected %v", tt.exts, tt.ext, result, tt.expected)
+			}
+		})
+	}
+}
